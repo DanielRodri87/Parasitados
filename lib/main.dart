bf7941666e96b318d02cd7bfe39ceb72f0679b26
DanielRodri87 <daniel.sousa@ufpi.edu.br>
2025-06-12 15:46:47 -0300

    corrigido para rodar via linux

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
-import 'login_page.dart'; 
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'dart:io';
+import 'login_page.dart';
 
-void main() {
+Future<void> main() async {
+  // Garante que o Flutter está inicializado
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Inicializa o sqflite para desktop
+  if (Platform.isLinux || Platform.isWindows || Platform.isMacOS) {
+    // Define o caminho para a biblioteca SQLite
+    sqfliteFfiInit();
+    databaseFactory = databaseFactoryFfi;
+  }
+
   runApp(const MyApp());
 }
 
