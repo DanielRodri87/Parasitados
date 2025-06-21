e37d2d7e09c8eca54d20904dd29cf74f734ccf21
JonasCGN <jonasbo66@gmail.com>
2025-06-21 18:56:24 -0300

    refactor: Update SplashScreen to use StatefulWidget and add loading progress indicator

--- lib/screen/questions/partials/roleta/roleta_screen.dart
+++ lib/screen/questions/partials/roleta/roleta_screen.dart
@@ -359,10 +359,10 @@ class RoletaScreenState extends State<RoletaScreen> with TickerProviderStateMixi
 										player2Animals.add(selectedAnimal!);
 									}
 								}
+								_checkForWinner();
 							} else {
 								qtdCorreta++;
 							}
-							_checkForWinner();
 						} else {
 							if (widget.modeGame == TypeModeGame.umJogador) {
 								isNotCorrect = true;
@@ -689,77 +689,81 @@ class RoletaScreenState extends State<RoletaScreen> with TickerProviderStateMixi
 		];
 		
 		return Container(
-			color: Colors.black.withAlpha((0.8 * 255).toInt()),
-			child: Stack(
-				children: [
-					Center(
-						child: Container(
-							margin: const EdgeInsets.all(32),
-							padding: const EdgeInsets.only(
-								left: 32,
-								right: 32,
-								top: 70,
-								bottom: 70
-							),
-							decoration: BoxDecoration(
-								gradient: LinearGradient(
-									colors: [
-										Color(0xffCD1A1A),
-										Color(0xff670D0D),
-									],
-									begin: Alignment.topLeft,
-									end: Alignment.bottomRight,
-								),
-								borderRadius: BorderRadius.circular(20),
-								boxShadow: [
-									BoxShadow(
-										color: Colors.black.withAlpha((0.3 * 255).toInt()),
-										blurRadius: 20,
-										offset: const Offset(0, 10),
-									),
-								],
-							),
-							child: Column(
-								mainAxisSize: MainAxisSize.min,
-								children: [
-									IconButton(
-										onPressed: (){
-											if(mounted) Navigator.of(context).pop();
-										}, 
-										icon: Icon(
-											Icons.close
-										)
-									),
-									widgetText[qtdIncorreta - 1],
-									const SizedBox(height: 30),
-									if(qtdIncorreta == 3)
-										ElevatedButton(
-											onPressed: () {
-												Navigator.restorablePopAndPushNamed(context, Routes.rankingScreen);
-											},
-											style: ElevatedButton.styleFrom(
-												backgroundColor: Colors.white,
-												foregroundColor: Colors.orange.shade400,
-												padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
-												shape: RoundedRectangleBorder(
-													borderRadius: BorderRadius.circular(25),
-												),
-												elevation: 5,
-											),
-											child: const Text(
-												'Ranking',
-											 style: TextStyle(
-													fontSize: 18,
-													fontWeight: FontWeight.bold,
-												),
-											),
-										)
-								],
-							),
-						),
+			decoration: BoxDecoration(
+				gradient: LinearGradient(
+					colors: [
+						Color(0xffCD1A1A),
+						Color(0xff670D0D),
+					],
+					begin: Alignment.topLeft,
+					end: Alignment.bottomRight,
+				),
+				borderRadius: BorderRadius.circular(20),
+				boxShadow: [
+					BoxShadow(
+						color: Colors.black.withAlpha((0.3 * 255).toInt()),
+						blurRadius: 20,
+						offset: const Offset(0, 10),
 					),
 				],
 			),
+			child: Column(
+				mainAxisSize: MainAxisSize.min,
+				  children: [
+					Row(
+						mainAxisAlignment: MainAxisAlignment.end,
+					  children: [
+					    IconButton(
+					    	onPressed: (){
+					    		if(mounted) Navigator.of(context).pop();
+					    	}, 
+					    	icon: Icon(
+					    		Icons.close,
+								color: Colors.white,
+					    	)
+					    ),
+					  ],
+					),
+				    Container(
+				    	padding: const EdgeInsets.only(
+				    		left: 32,
+				    		right: 32,
+				    		top: 70,
+				    		bottom: 70
+				    	),
+				    	
+				    	child: Column(
+				    		mainAxisSize: MainAxisSize.min,
+				    		children: [
+				    			widgetText[qtdIncorreta - 1],
+				    			const SizedBox(height: 30),
+				    			if(qtdIncorreta == 3)
+				    				ElevatedButton(
+				    					onPressed: () {
+				    						Navigator.restorablePopAndPushNamed(context, Routes.rankingScreen);
+				    					},
+				    					style: ElevatedButton.styleFrom(
+				    						backgroundColor: Colors.white,
+				    						foregroundColor: Colors.orange.shade400,
+				    						padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 16),
+				    						shape: RoundedRectangleBorder(
+				    							borderRadius: BorderRadius.circular(25),
+				    						),
+				    						elevation: 5,
+				    					),
+				    					child: const Text(
+				    						'Ranking',
+				    					 style: TextStyle(
+				    							fontSize: 18,
+				    							fontWeight: FontWeight.bold,
+				    						),
+				    					),
+				    				)
+				    		],
+				    	),
+				    ),
+				  ],
+				),
 		);
 	}
 
